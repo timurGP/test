8c07a2f4dac4313b9bcb7cbe627b2bfb6e987ed2
Timur <timur1346@yandex.ru>
2022-03-20 02:42:48 +0300

    Первый комит

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+// alert('Hello world')
+
+// let answer = confirm('Ты тут?');
+
+ //console.log(answer)
+
+// let answer = prompt('Ты тут?', 'Да');
+
+// console.log(typeof(null));
+
+ //console.log('arr' + '- object');
+ //console.log(4 + ' - object')
+
+ let incr = 10,
+     decr = 10;
+
+     console.log(incr++);
+     console.log(decr--);
+
+     console.log(5%7);
+     console.log('2' === 2);
+
+     let isChecked = false,
+         isClose = false;
+
+         console.log(isChecked || isClose);
